8d8dd888ed4862d8bebf5be605515f8941bebc91
Mauricio Borges <mvrborges0@gmail.com>
2019-09-05 19:52:57 -0300

    initial commit with project unity 2019b isometric

---
+++ Assets/Scripts/Movimento.cs
@@ -0,0 +1,31 @@
+﻿using Assets.scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimento : MonoBehaviour
+{
+    public float movementSpeed = 1f;
+    IsometricoRenderer isoRenderer;
+    Rigidbody2D rbody;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+        isoRenderer = GetComponentInChildren<IsometricoRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 currentPos = rbody.position;
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(horizontalInput, verticalInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * movementSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        isoRenderer.SetDirection(movement);
+        rbody.MovePosition(newPos);
+    }
+}
