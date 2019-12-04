13f621347997d47f38a5e5c463250b2377c1efc2
vsleandro <54065123+vsleandro@users.noreply.github.com>
2019-12-04 07:53:13 -0300

    added reference on enemy code

--- Assets/Scripts/EnemyMoviment.cs
+++ Assets/Scripts/EnemyMoviment.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class EnemyMoviment : MonoBehaviour
 {
+    //https://github.com/ribbybibby/Unity-Learn/blob/master/SwitchBlade/Scripts/EnemyMovement.cs
     public float speed;
     private Transform target;
 
-
+   
     public static readonly string[] staticDirections = { "Static N", "Static NW", "Static W", "Static SW", "Static S", "Static SE", "Static E", "Static NE" };
     public static readonly string[] runDirections = { "Run N", "Run NW", "Run W", "Run SW", "Run S", "Run SE", "Run E", "Run NE" };
 
